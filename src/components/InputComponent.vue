2135bafe47e79cde22e9e2dcb426e49b7c873b84
Pietrodon <donvitopietro00@gmail.com>
2022-03-20 01:59:46 +0100

    creazione funzione per richiamo api e inserimento ascolto click

--- src/components/InputComponent.vue
+++ src/components/InputComponent.vue
@@ -1,14 +1,13 @@
 <template>
     <div id="app">
-        <div>
-            <input type="text" placeholder="Cerca Film" v-model="search">
-            <button>Cerca</button>
+        <div class="search-bar">
+            <input type="text" placeholder="Cerca Film" v-model="search" @keyup.enter="richiamoApi">
+            <button @click="richiamoApi">Cerca</button>
         </div>        
-        <div v-for="movie in movies" :key="movie.id">
+        <div v-for="movie in movies" :key="movie.id" class="movie">
             {{movie.title}}
         </div>
-
-        
+          
     </div>
 </template>
 <script>
@@ -20,26 +19,35 @@ export default {
   name: 'App',
   data() {
       return{
-          search:'ritorno',
+          search:'',
           movies:[],   
       }   
   },
-  created(){
-
-      axios.get('https://api.themoviedb.org/3/search/movie/?api_key=d6d1e144bc9bbed54e5110613577c788&query=batman')
+  methods:{
+      richiamoApi: function() {
+      axios.get(`https://api.themoviedb.org/3/search/movie/?api_key=d6d1e144bc9bbed54e5110613577c788&query=${ this.search }`)
       .then(res =>{
           console.log(res.data)
           this.movies = res.data.results
       })
-
   } 
-
+  }
 }
 </script>
 
 
 
 <style scoped lang="scss">
+.search-bar{
+    background-color: black;
+    
+}
+.movie{
+    background: grey;
+    position: flex;
+    color:green
+}
+
 
 </style>
 
